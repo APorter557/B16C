29b36ab7a09d389795a0377b73427c5c225089af
APorter557 <adam.a.porter@hotmail.com>
2026-02-27 16:52:23 +0000

    Assignment finished - had a little trouble with compiler linking, but I think this is solved by the cmake functionality that I am yet to understand

--- part-4/shortest_paths_decode.hpp
+++ part-4/shortest_paths_decode.hpp
@@ -8,6 +8,16 @@
 inline std::vector<int> decode(const std::vector<hop_t> &DP, int v)
 {
     // WRITE YOUR CODE HERE
+    std::vector<int> path;
+    int u = v;
+
+    while (DP[u].vertex != -1) {
+        path.push_back(u);
+        u = DP[u].vertex;
+    }
+    path.push_back(u);
+    std::reverse(path.begin(), path.end());
+    return path;
 }
 
 #endif // __shortest_paths_decode__
--- part-4/shortest_paths_sparse.cpp
+++ part-4/shortest_paths_sparse.cpp
@@ -10,6 +10,36 @@ std::vector<hop_t> bellman_ford(const SparseGraph &graph, const int source,
     auto DP = std::vector<hop_t>(V, {inf, -1});
 
     // WRITE YOUR CODE HERE
+    has_negative_cycle = false;
+    assert(0 <= source && source < V);
+    DP[source].weight = 0;
+    for (int iter = 0; iter < V-1; ++iter) {
+        for (int r = 0; r < V; ++r) {
+            for (const auto &hop : graph[r]) {
+                int v = hop.vertex;
+                float w = hop.weight;
+                if ((DP[r].weight + w < DP[v].weight) && (DP[r].weight != inf)) {
+                    DP[v] = {DP[r].weight + w, r};
+                }
+            }
+        }
+    }
+
+    // Checks for negative cycle by trying to relax one more time (theoretically V-1 relaxations is the max if no negative cycles present)
+    for (int r = 0; r < V; ++r) {
+        for (const auto &hop : graph[r]) {
+            int v = hop.vertex;
+            float w = hop.weight;
+            if ((DP[r].weight + w < DP[v].weight) && (DP[r].weight != inf)) {
+                has_negative_cycle = true;
+                break;
+            }
+        }
+        if (has_negative_cycle) {
+            break;
+        }
+    }
+
 
     return DP;
 }
@@ -28,6 +58,34 @@ std::vector<hop_t> dijkstra(const SparseGraph &graph, const int source)
     auto DP = std::vector<hop_t>(graph.size(), {inf, -1});
 
     // WRITE YOUR CODE HERE
+    DP[source].weight = 0;
+
+    std::vector<bool> visited(graph.size(), false);
+
+    for (int a = 0; a < (signed)graph.size(); ++a) {
+        int r = -1;
+        float min_d = inf;
+        for (int i = 0; i < (signed)graph.size(); ++i) {
+            if (!visited[i] && DP[i].weight < min_d) {
+                min_d = DP[i].weight;
+                r = i;
+            }
+        }
+        if (r == -1) {
+            break; // No more reachable vertices
+        }
+        visited[r] = true;
+
+        for (const auto &hop : graph[r]) {
+            int v = hop.vertex;
+            float w = hop.weight;
+            if (!visited[v] && DP[r].weight + w < DP[v].weight) {
+                DP[v] = {DP[r].weight + w, r};
+            }
+        }
+    }
+
+
 
     return DP;
 }
